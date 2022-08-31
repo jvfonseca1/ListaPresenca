9729384ebd3779f3cd00bb72f9cfe8c1b510bbec
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2022-08-31 17:14:36 -0300

    Add Enter Button Function

--- ListaPresença/src/pages/Home/index.jsx
+++ ListaPresença/src/pages/Home/index.jsx
@@ -1,25 +1,51 @@
 import React, {useState} from 'react';
-import './styles.css';
 
+import './styles.css';
 import {Card} from '../../components/Card';
 
 export function Home() {
-  const [studentName, setStudentName] = useState();
-  return (
-    <div className='container'>
-      <h1>Lista de Presença:</h1>
-      <input 
-        type="text" 
-        placeholder="Digite o nome"
-        onChange={e => setStudentName(e.target.value)}
-      />
-      <button type="button"> Adicionar</button>
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
 
-      <Card name="João Victor" time="13:23" />
-      <Card name="Rodrigo" time="10:55" />
-      <Card name="Ana" time="12:10" />
+  // Função Adicionar Estudantes 
+  function handleAddStudent(){
+    const newStudent ={
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+    setStudents(prevState => [...prevState,newStudent]);
+  }
+
+  //Script Reconhecer Botão Enter
+  addEventListener('keypress', function(e) {
+    if(e.key === "Enter"){
+      const btn = document.getElementById("addStudent");
+      btn.click();
+    }  
+  })
+
+  return ( 
+    <div className='container'>
+      <h1>Lista de Presença</h1>
+      <input 
+          type="text" 
+          placeholder="Digite o nome"
+          onChange={e => setStudentName(e.target.value)
+        }
+      />
+      <button
+      id='addStudent'
+      type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+      {
+        students.map(student => <Card name={student.name} time={student.time} />)
+      }
+      
     </div>
   )
 }
-
-
